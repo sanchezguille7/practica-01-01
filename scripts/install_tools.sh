830bfcb949b5421c85c39895e7fc74beb111b9e2
Ubuntu <ubuntu@ip-172-31-50-105.ec2.internal>
2023-10-11 12:30:49 +0000

    Añadimos el GoAcces

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,20 +3,16 @@
 #Muestra todos los comandos que se van ejecutadno
 set -x
 
-# Configuramos las variables
-#-----------------------------------------------------
-PHPMYADMIN_APP_PASSWORD=123456
-APP_USER=Guille
-APP_PASSWORD=1234
-#-----------------------------------------------------
+# Paso1. Se importan las variables de configuracion
+source .env
 
 # Actualizamos los repositorios
 apt update
 
 # actualizamos los paquetes 
-#apt upgrade -y
+apt upgrade -y
 
-# Configuramos las respuestas de la instalaion de phpMyAdmin
+# Paso2. Configuramos las respuestas de la instalaion de phpMyAdmin
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password $PHPMYADMIN_APP_PASSWORD" | debconf-set-selections
@@ -29,3 +25,36 @@ apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
 mysql -u root <<< "DROP USER IF EXISTS '$APP_USER'@'%'"
 mysql -u root <<< "CREATE USER '$APP_USER'@'%' IDENTIFIED BY '$APP_PASSWORD';"
 mysql -u root <<< "GRANT ALL PRIVILEGES ON *.* TO '$APP_USER'@'%';"
+
+# instalar adminer
+# creamos directorio para adminer
+mkdir -p /var/www/html/adminer
+
+# Descargamos el archivo de adminer
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php -P /var/www/html/adminer
+
+#Renombramos el nombre del archivo adminer
+mv /var/www/html/adminer/adminer-4.8.1-mysql.php /var/www/html/adminer/index.php
+
+# Modificamos l propietario del directorio /var/www/html
+chown -R www-data:www-data /var/www/html
+
+# Descaragamos GoAcces
+apt install goaccess -y
+
+# Iniciar GoAcces
+
+#Creamos un directorio paralos informes html de GoAcces
+mkdir -p /var/www/html/stats
+
+# ejecutamos GoAcces en sugundo plano
+sudo goaccess /var/log/apache2/access.log -o /var/www/html/stats/index.html --log-format=COMBINED --real-time-html --daemonize
+
+#Creamos el archivo .htpasswd
+htpasswd -bc /etc/apache2/.htpasswd $STATS_USERNAME $STATS_PASSWORD
+
+# Copiamos el archivo de configurcion de apache con la configuracion del acceso al directorio
+cp ../conf/000-default-stats.conf /etc/apache2/sites-available/000-default.conf
+
+# Reiniciamos el servidor apache
+systemctl restart apache2
